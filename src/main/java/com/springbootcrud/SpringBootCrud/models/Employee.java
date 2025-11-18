176f4ad6c499cddc78ac37e94961e415b677ef08
unknown <exequieldbaldini@hotmail.com>
2025-11-18 16:53:58 -0300

    initial setup of springboot CRUD app

---
+++ src/main/java/com/springbootcrud/SpringBootCrud/models/Employee.java
@@ -0,0 +1,85 @@
+package com.springbootcrud.SpringBootCrud.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="employees")
+
+
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String dob;
+
+    private String gender;
+
+    public Employee() { }
+
+
+
+    public Employee(int id, String firstName, String lastName, String email, String dob, String gender) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.dob = dob;
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
